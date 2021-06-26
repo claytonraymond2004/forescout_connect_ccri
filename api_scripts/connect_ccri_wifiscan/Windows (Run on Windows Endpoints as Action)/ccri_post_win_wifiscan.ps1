c8cb1b335f58cabb907eac20517b3b1c3f76b170
Clay Colwell <claytonraymond2004@gmail.com>
2021-06-26 12:33:01 -0400

    post to API

--- api_scripts/connect_ccri_wifiscan/Windows (Run on Windows Endpoints as Action)/ccri_post_win_wifiscan.ps1
+++ api_scripts/connect_ccri_wifiscan/Windows (Run on Windows Endpoints as Action)/ccri_post_win_wifiscan.ps1
@@ -1,4 +1,52 @@
-﻿# Get Wifi networks from windows
+﻿<#
+.SYNOPSIS
+    Enumerates WiFi networks discovered by local computer and POSTs it to Forescout CCRI Connect App connect_ccri_wifiscan property
+
+.DESCRIPTION
+    USes netsh to find local WiFi networks and processes to get each BSSID and signal information, authenticates to Forescout Connect Web API, and then updates host data
+
+.PARAMETER ip
+    The IP of the host to update in Forescout (the device this is run on)
+
+.PARAMETER api
+    IP address of the Connect CCRI App API
+
+.PARAMETER username
+    Username for the Connect CCRI App API
+
+.PARAMETER password
+    Password for the Connect CCRI App API
+    
+.SWITCH inseucre
+    Will ignore certificate validation to Forescout Web API
+#>
+
+Param(
+    [string]$ip = $(throw "-ip is required."),
+    [string]$api = $(throw "-api is required."),
+    [string]$username = $(throw "-username is required."),
+    [string]$password = $(throw "-password is required."),
+    [switch]$insecure = $false
+)
+
+# Create type for ignoring certs if specified
+add-type @"
+    using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+    public class TrustAllCertsPolicy : ICertificatePolicy {
+        public bool CheckValidationResult(
+            ServicePoint srvPoint, X509Certificate certificate,
+            WebRequest request, int certificateProblem) {
+            return true;
+        }
+    }
+"@
+# Disable Certificate Validation in Powershell if specified
+if ($insecure -eq $true) {
+    [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+}
+
+# Get Wifi networks from windows
 $WiFi = (netsh wlan show network  mode=bssid |  Select-Object -Skip  3).Trim()  | Out-String
 
 # Regex for parsing data -- lump all the BSSIDs in a property we'll expand out later
@@ -54,3 +102,30 @@ $WiFi -split  "\r\s+\n"  | ForEach {
 }
 
 $WiFiNetworks | ConvertTo-Json
+
+# Get Connect Web API Token
+try {
+    $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $headers.Add("Content-Type", "application/json")
+    $body = "{`"username`": `"$username`", `"password`": `"$password`", `"app_name`": `"ccri`", `"expiration`": `"1`"}"
+    $response = Invoke-RestMethod "https://$api/connect/v1/authentication/token" -Method 'POST' -Headers $headers -Body $body
+
+    # POST to /hosts API To put data
+    try {
+        $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+        $headers.Add("Content-Type", "application/json")
+        $headers.Add("Authorization", "Bearer $($response.data.token)")
+        $headers.Add("Accept", "application/json")
+        $body = "{ `"ip`": `"$ip`", `"properties`":{ `"connect_ccri_wifiscan`": $(ConvertTo-Json -Compress $WiFiNetworks) }}"
+        $response = Invoke-RestMethod "https://$api/connect/v1/hosts" -Method 'POST' -Headers $headers -Body $body
+    } catch {
+        # Error POSTing Data
+        Write-Error "Forescout Connect ccri App API POST Error"
+        Write-Error $_
+    }
+
+} catch {
+    # Error authenticating
+    Write-Error "Forescout Connect API Auth Error"
+    Write-Error $_
+}
