b60eb90c59e68e7f935cc43a06079fc9ce3d7217
Clay Colwell <claytonraymond2004@gmail.com>
2022-03-24 17:14:59 -0400

    use web api call to get groups

--- app/property_cybercom_guess.py
+++ app/property_cybercom_guess.py
@@ -1,10 +1,19 @@
+import json
+import urllib
+
 # Hold response to Forescout EyeExtend Connect
 response = {
     "properties": {}
 }
 
+## NOTE: For whatever reason, Connect won't pass the in-group property to this script. Workaround using Web API call
+
 # Get parameters
-groups = params.get("in-group") # device groups; only provided if triggered off group change and calculaing guessed category
+groups_params = params.get("in-group") # device groups; only provided if triggered off group change and calculaing guessed category
+host_ip = params["ip"] # Host IP address -- what host are we resolving?
+# Get Forescout OIM Web API Details (We connect to OIM to get data) from Connect App context
+forescout_url = params["connect_ccri_forescout_url"]
+forescout_jwt_token = params["connect_authorization_token"]
 
 # Device category groups; for performing guess
 mobile = ["P-Mobile", "P-Mobile Devices Manual Discover", "P-Mobile Devices Manual Discover", "C2C-Step1-Manual-Mobile Device", "C2C-Step1-Discovery-Mobile"]
@@ -13,11 +22,38 @@ workstations = ["P-Windows", "P-Linux/Unix", "P-macOS", "P-Windows Manual Discov
 cps = ["P-OT Devices","P-OT Devices Manual Discover","C2C-Step1-CPS/CS","C2C-Step1-Manual-CPS/CS Device"]
 iot = ["P-IoT", "P-IoT Devices Manual Discover", "C2C-Step1-IOT", "C2C-Step1-Manual-IOT Device"]
 
-#Resolving the guess value
-logging.debug(groups)
-guess = None
+# Create request to get host data from Forescout
+forescout_headers = {"Authorization": forescout_jwt_token}
+forescout_request = urllib.request.Request(forescout_url + "/api/hosts/ip/" + host_ip + "/?fields=online", headers=forescout_headers)
 
-response["properties"]["connect_ccri_disa_reporting_cybercom_cat_guess"] = guess
+logging.debug("Preparing to get host information for host: {}".format(host_ip))
 
-response["succeeded"] = True
-## DONE
+try:
+    # Make API request to Forescout Web API For host
+    forescout_resp = urllib.request.urlopen(forescout_request, context=ssl_context) # To use the server validation feature, use the keyword 'ssl_context' in the http reqeust
+    if forescout_resp.getcode() == 200:
+        logging.debug("Received data from Forescout Web API")
+        # Load response data
+        host_data = json.loads(forescout_resp.read().decode('utf-8'))
+
+        groups_web = host_data.get('host', {}).get('fields', {}).get('in-group', None)
+
+        if groups_web:
+            logging.debug(groups_web)
+            #Resolving the guess value
+            guess = None
+            
+            if guess:
+                response["properties"]["connect_ccri_disa_reporting_cybercom_cat_guess"] = guess
+
+        else:
+            logging.error("No Group data for host found! (in-group is not found in Forescout Web API)")
+            response["error"] = "No Group data for host found, cannot guess USCYBERCOM Category! (in-group is not found in Forescout Web API)"
+
+    else:
+       logging.error("Failed API Request to Forescout to get host data!")
+       response["error"] = "Failed API request to Forescout Web API server!"
+
+except Exception as e:
+    logging.error("General Block Exception: {}".format(e))
+    response["error"] = "Exception! Check the debug logs for more info."
